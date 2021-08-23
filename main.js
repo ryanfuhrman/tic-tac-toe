702f61017a69da71c8bd345bce715998a7ef9711
ryanfuhrman <rsfuhrman10@gmail.com>
2021-08-22 20:16:36 -0400

    messy messy code

--- main.js
+++ main.js
@@ -1,24 +1,71 @@
 const board = document.querySelector(".gameboard");
 
 const Gameboard = (() => {
+  let board = [
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+    { empty: true, player: "" },
+  ];
+
+  function updateSquare(index, player) {
+    console.log(!this.board[index.empty]);
+    return (
+      (this.board[index] = {
+        empty: false,
+        player,
+      }),
+      Game.populateBoard()
+    );
+  }
+
   return {
-    gameboard: [
-      { emtpy: false, content: "x" },
-      { emtpy: false, content: "o" },
-      { emtpy: false, content: "" },
-      { emtpy: false, content: "" },
-      { emtpy: false, content: "x" },
-      { emtpy: false, content: "" },
-      { emtpy: false, content: "" },
-      { emtpy: false, content: "o" },
-      { emtpy: false, content: "x" },
-    ],
+    board,
+    updateSquare,
   };
 })();
 
-const DisplayController = (() => {
+const Game = (() => {
+  let turn = true;
+  let player;
+
+  if (turn === true) {
+    player = "x";
+  } else {
+    player = "o";
+  }
+
+  board.addEventListener("click", (e) => {
+    let square = e.target.classList[1];
+    if (Gameboard.board[square].empty === true) {
+      Gameboard.updateSquare(square, player);
+      !turn;
+    } else {
+      console.log(
+        "Sorry, this square was already chosen. Pick another square!"
+      );
+    }
+
+    console.log(Gameboard);
+  });
+
+  function populateBoard() {
+    board.innerHTML = "";
+    Gameboard.board.map((square, index) => {
+      const squareLi = document.createElement("li");
+      squareLi.className = `squares ${index}`;
+      squareLi.innerHTML = `${square.player}`;
+      board.appendChild(squareLi);
+    });
+  }
+
   return {
-    turn: true,
+    populateBoard,
   };
 })();
 
@@ -26,11 +73,4 @@ const Player = (name) => {
   return { name };
 };
 
-(populateBoard = () => {
-  Gameboard.gameboard.map((square, index) => {
-    const squareLi = document.createElement("li");
-    squareLi.className = `squares ${index}`;
-    squareLi.innerHTML = `${square.content}`;
-    board.appendChild(squareLi);
-  });
-})();
+Game.populateBoard();
